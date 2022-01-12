31fc31f04f2a7a4b06d4c66d0082869093f0e417
dingsheng <727040141@qq.com>
2022-01-12 10:57:16 +0800

    feat: change shell

--- weather.sh
+++ weather.sh
@@ -9,5 +9,10 @@ UA="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Ge
 curl \
   -H "Accept-Language: $LANGUAGE" \
   -H "User-Agent: $UA" \
-  -o result.html \
-  wttr.in/$CITY?format=4\&$UNIT
+  -o result.png \
+  wttr.in/$CITY
+
+
+# wttr.in/$CITY?format=4\&$UNIT
+
+#   -o result.html \
